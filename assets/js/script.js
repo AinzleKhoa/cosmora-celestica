6537c61785ff410aca0cf5a4e176fc352e3c5f78
AinzleKhoa <khoale1855@gmail.com>
2025-05-18 22:05:15 +0700

    CTA, button up added. Responsiveness for all. Finshing up the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,6 +118,23 @@ function hidePreloader(minTime = 1000) {
     }, delay);
 }
 
+const scrollBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {  // Show after 300px scroll
+        scrollBtn.style.display = 'block';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
 /*---------------------------------------
 // Main initializer function
 -----------------------------------------*/
